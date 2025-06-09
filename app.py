cec6205f2dd4a84f2cdefdc57553ed1ca3257583
CimiChin <151364311+CimiChin@users.noreply.github.com>
2025-06-09 10:39:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+# app.py
+import streamlit as st
+from multiapp import MultiApp
+from pages import eda, model_results, prediction
+
+app = MultiApp()
+
+app.add_app("EDA & Dataset", eda.app)
+app.add_app("Model Results", model_results.app)
+app.add_app("Predict Demand", prediction.app)
+
+app.run()
 
