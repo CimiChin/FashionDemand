4a37883baf43ebc335b168facbb0e0e7d06aed05
CimiChin <151364311+CimiChin@users.noreply.github.com>
2025-06-09 10:38:00 +0700

    Update multiapp.py

--- multiapp.py
+++ multiapp.py
@@ -1,1 +1,12 @@
+class MultiApp:
+    def __init__(self):
+        self.apps = []
+
+    def add_app(self, title, func):
+        self.apps.append({"title": title, "function": func})
+
+    def run(self):
+        import streamlit as st
+        app = st.sidebar.selectbox("Select Page", self.apps, format_func=lambda app: app["title"])
+        app["function"]()
 
